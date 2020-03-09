bcef703863cd50e1fbdb84ab3262dbf236141af2
Bryan Graeser <bsg68@cornell.edu>
2020-03-08 22:09:00 -0400

    fixed naming

--- desktop/src/com/fallenflame/game/desktop/DesktopLauncher.java
+++ desktop/src/com/fallenflame/game/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
-package com.mygdx.game.desktop;
+package com.fallenflame.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
+import com.fallenflame.game.GDXRoot;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+		new LwjglApplication(new GDXRoot(), config);
 	}
 }
